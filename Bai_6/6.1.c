4257503a4ac423e286021f3ed809ac4ba001546b
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2022-07-25 22:34:22 +0700

    Update 6.1.c

--- Bai_6/6.1.c
+++ Bai_6/6.1.c
@@ -3,8 +3,11 @@
 int main()
 {
     char s[100], s1[100];
-    fflush(stdin); // thật ra mình cũng k hiểu cái lệnh này để làm gì
-    fgets(s, sizeof(s), stdin); //dùng cái này t
+    fflush(stdin); 
+    /*Khi nhập xâu thì kết thúc bằng một ký tự xuống dòng ‘\n’. 
+    Nhưng compiler vẫn nhận đây là một xâu cho tới bộ nhớ tạm thời và truyền nó vào biến xâu tiếp theo của chương trình.
+    fflush(stdin); với mục đích xóa đi bộ nhớ tạm thời của chương trình để không tồn tại những giá trị rác như bên trên.*/
+    fgets(s, sizeof(s), stdin); 
     fgets(s1, sizeof(s1), stdin);
     strcat(s, s1);
     int dem, n = strlen(s);
