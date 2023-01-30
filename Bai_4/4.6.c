db9504891ba431a76d15c17ebab7d4e6e19796c9
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-01-30 11:33:28 +0700

    Update 4.6.c
    Update problem

--- Bai_4/4.6.c
+++ Bai_4/4.6.c
@@ -1,3 +1,41 @@
+Viết chương trình tính các tổng sau: 
+    S=1+x+x2+x3+⋯+xn
+Trong đó n là một số nguyên dương và x là một số thực bất kỳ được nhập từ bàn phím khi chạy chương trình. 
+Chú ý: Kiểm tra dữ liệu đầu vào của chương trình, nếu không đúng thì kết thúc chương trình và in ra màn hình Error.
+Mô tả dữ liệu đầu vào:
+Số nguyên dương n
+Số thực x
+
+Mô tả dữ liệu đầu ra:
+Giá trị của S
+
+For example:
+    Dữ liệu đầu vào	|Kết quả
+    1               |2.500000
+    1.5             |
+
+// All in main prog
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double S=0, x;
+    int n, i;
+    
+    scanf("%d", &n);
+    scanf("%lf", &x);
+    if(n<0){
+        printf("Error");
+        return 0;
+    }
+    for(i=0;i<=n;i++){
+        S=S+pow(x, i);
+    }
+    printf("%lf\n", S);
+}
+
+
+//Module Prog
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
