33f617ac4060bd022002869dbb2162e309652bff
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-01-31 08:34:42 +0700

    Update 4.7.c
    update

--- Bai_4/4.7.c
+++ Bai_4/4.7.c
@@ -1,24 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+Đề bài:
+Viết chương trình tính các tổng sau: 
+S=1−x+x^2−x^3+⋯+(−1)^n×x^n
+Trong đó n là một số nguyên dương và x là một số thực bất kỳ được nhập từ bàn phím khi chạy chương trình. 
+Chú ý: Kiểm tra dữ liệu đầu vào của chương trình, nếu không đúng thì kết thúc chương trình và in ra màn hình Error.
+Mô tả dữ liệu đầu vào:
+Số nguyên dương n
+Số thực x
 
-int main()
-{
-    double S = 0, x;
-    int n, i = 1;
+Mô tả dữ liệu đầu ra:
+Giá trị của S
 
-    scanf("%lf", &x);
-    scanf("%d", &n);
-    if (n < 1 || x < 0)
-    {
-        printf("Error");
-        exit(0);
-    }
-    for (i = 1; i <= n; i++)
-        {
-            S = sqrt(x + S);
-        }
+For example:
+Dữ liệu đầu vào	    Kết quả
+1               |-0.500000
+1.5             |
+
+
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double S=0, x;
+    int n, i;
     
-    printf("%.4lf", S);
-    return 0;
+    scanf("%d", &n);
+    scanf("%lf", &x);
+    if(n<0){
+        printf("Error");
+        return 0;
+    }
+    for(i=0;i<=n;i++){
+        S=S+pow(-1, i)*pow(x, i);
+    }
+    printf("%lf\n", S);
 }
