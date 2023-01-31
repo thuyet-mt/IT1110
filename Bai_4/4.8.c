8372ff346d7d8ca48b0ddd800917145f74e8793d
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-01-31 08:50:39 +0700

    Update 4.8.c
    Update problem

--- Bai_4/4.8.c
+++ Bai_4/4.8.c
@@ -1,3 +1,23 @@
+Đề bài:
+Viết chương trình tính các tổng sau: 
+    S=1+x/1!+x^2/2!+x^3/3!+⋯+x^n/n!
+Trong đó n là một số nguyên dương và x là một số thực bất kỳ được nhập từ bàn phím khi chạy chương trình. 
+Chú ý: Kiểm tra dữ liệu đầu vào của chương trình, nếu không đúng thì kết thúc chương trình và in ra màn hình Error.
+
+Mô tả dữ liệu đầu vào:
+    Số nguyên dương n
+    Số thực x
+
+Mô tả dữ liệu đầu ra:
+    Giá trị của S
+
+For example:
+            Dữ liệu đầu vào	| Kết quả
+            1               | 2.500000
+            1.5             |
+            
+
+
 #include<stdio.h>
 #include<math.h>
 int factorial(int a){
