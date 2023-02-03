218837cceec750feaa1f6becfb975a55e7d42302
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-02-03 22:18:35 +0700

    Update 5.2.c
    Update problem and solution

--- Bai_5/5.2.c
+++ Bai_5/5.2.c
@@ -1,15 +1,38 @@
+Đề bài:
+Nhập mảng số nguyên từ bàn phím. Tính trung bình cộng các số âm, tổng các số dương và đưa kết quả ra màn hình.
+Mô tả giá trị đầu vào:
+- Dòng đầu tiên cho biết số phần tử mảng 𝑛.
+- Dòng thứ 2 chứa giá trị của n phần tử mảng, các phần tử cách nhau một dấu cách.
+
+Mô tả giá trị đầu ra:
+Một dòng gồm 2 giá trị cách nhau 1 dấu cách:
+- Giá trị trung bình cộng các số âm. nếu mảng chỉ toàn số âm, in ra màn hình Mảng không có số dương.
+- Tổng các số dương, nếu mảng chỉ toàn số dương in ra màn hình Mảng không có số âm.
+
+For example:
+|===================|=================================|
+|Dữ liệu đầu vào    |Kết quả                          |
+|===================|=================================|
+|2                  |-492.000000 295.000000           |
+|295 -492           |                                 |
+|===================|=================================|
+|5                  |Mang khong co so am 15.000000    |
+|1 2 3 4 5          |                                 |
+|===================|=================================|
+|2                  |-1.000000 Mang khong co so duong |
+|-1 -1              |                                 |
+|===================|=================================|
+
+Có thể làm theo cách define ở bài 1, tuy nhiên mình thích dùng malloc hơn :D 
+    
 #include <stdio.h>
 #include <stdlib.h>
-struct Array
-{
-    int n;
-    int *A;
-};
+
 
 int main()
 {
     int n, i = 0, m=0, m1=0;
-    struct Array arr;
+    int *A;
     double S=0, S1=0;
     scanf("%d", &n);
     if (n < 0)
@@ -17,23 +40,25 @@ int main()
         printf("Error");
         exit(0);
     }
-    arr.A = (int*)malloc(n * sizeof(int));
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &arr.A[i]);
+    A = (int*)malloc(n * sizeof(int));
+    for (i = 0; i < n; i++){
+        scanf("%d", &A[i]);
     }
+    // Tính tổng các phần tử < 0
     for (i=0;i<n;i++){
-        if(arr.A[i]<0){
-            S=S+arr.A[i];
-            m++;
+        if(A[i]<0){
+            S=S+A[i];
+            m++; // cứ có 1 phần tử < 0 thì m sẽ tăng lên 1 đơn vị, tức là có bao nhiêu phần tử < 0 thì m sẽ bằng bấy nhiêu
         }
     }
+    // Tính tổng các phần tử > 0
     for (i=0;i<n;i++){
-        if(arr.A[i]>0){
-            S1=S1+arr.A[i];
-            m1++;
+        if(A[i]>0){
+            S1=S1+A[i];
+            m1++; // tương tự m
         }
     }
+    
     if(m==0){
         printf("Mang khong co so am ");
         printf("%lf", S1);
@@ -47,5 +72,5 @@ int main()
         printf("%lf ", S/m);
         printf("%lf", S1);
     }
-    
+    free(A);
 }
