cef918b5738f7f974419591c27b43ce04c8ac4ac
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-02-04 13:40:38 +0700

    Update 5.4.c
    update problem & solution

--- Bai_5/5.4.c
+++ Bai_5/5.4.c
@@ -1,35 +1,61 @@
+Đề bài
+Viết chương trình cho phép nhập vào 1 mảng số nguyên (tối đa 10 phần tử), in ra màn hình tích của 2 phần tử liên tiếp lớn nhất. Nếu mảng chỉ có 1 phần tử trả về kết quả là 0. Nếu tích lớn nhất là một số âm thì trả về 0.
+Mô tả giá trị đầu vào:
+- Dòng đầu tiên chứa số nguyên n: số phần tử của mảng (2 <= n <=10)
+- Dòng thứ 2 chứa danh sách các số cách nhau khoảng trắng
+Mô tả giá trị đầu ra:
+Tích 2 phần tử liên tiếp lớn nhất.
+
+For example:
+Dữ liệu đầu vào	    |Kết quả đúng
+5                   |27
+1 9 3 2 6           |
+====================|=============                    
+4                   |18
+-3 -6 2 4           |
+====================|=============                            
+1                   |0
+10                  |
+====================|=============         
+2                   |45
+9 5                 |
+====================|=============         
+5                   |0
+1 -8 2 -3 4         |
+
+
+Cách làm: bài này mình lấy ý tưởng xét 2 phần tử liên tiếp của thuật toán bubble sort 
+
 #include <stdio.h>
 #include <stdlib.h>
-struct Array
-{
-    int n;
-    int *A;
-};
 
 int main()
 {
-    int n, i = 0, j=0, temp=0;
-    struct Array arr;
-    
+    int n, i = 0, j = 0, temp = 0;
+    int *A;
+
     scanf("%d", &n);
-    if (n < 0&&10<n)
+    if (n < 0 && 10 < n)
     {
         printf("Error");
-        exit(0);
+        return 0;
     }
-    arr.A = (int*)malloc(n * sizeof(int));
+    A = (int *)malloc(n * sizeof(int));
     for (i = 0; i < n; i++)
     {
-        scanf("%d", &arr.A[i]);
+        scanf("%d", &A[i]);
     }
-    for(i=0;i<n;i++){
-        for(j=0;j<n-1-i;j++){ // tại sao lại có -i? 
-        //vì sau mỗi lần lặp, số cặp cần so sánh lại giảm đi 1, do phần tử cuối cùng đã nằm đúng vị trí
-            if(arr.A[j]*arr.A[j+1]>=temp){
-                temp=arr.A[j]*arr.A[j+1];
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - 1 - i; j++)
+        {   // tại sao lại có -i?
+            // vì sau mỗi lần lặp, số cặp cần so sánh lại giảm đi 1, do phần tử cuối cùng đã nằm đúng vị trí
+            if (A[j] * A[j + 1] >= temp)
+            {
+                temp = A[j] * A[j + 1];
             }
         }
     }
-   
-        printf("%d ", temp);
+
+    printf("%d ", temp);
 }
