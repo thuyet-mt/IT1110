c09d3b532ecbc8039b68d4857404e670ced01601
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-02-06 10:23:26 +0700

    Create Bai4.c

---
+++ Midterm & Final Test Code/Bai4.c
@@ -0,0 +1,16 @@
+Viết chương trình nhập vào 1 dãy N số nguyên cho trước. Tìm tổng và trung bình của các phần tử cực đại.
+(Phần tử cực đại là phần tử lớn hơn phần tử ngay trước và ngay sau nó).
+Ví dụ: các phần tử được tô đậm dưới đây là phần tử cực đại:
+1 5 2 6 3 5 1 8 8
+Chú ý:
+Mô tả giá trị đầu vào:
+- Số nguyên dương N
+- Mảng N số nguyên
+Mô tả giá trị đầu ra:
+- Tổng các phần tử cực đại
+- Trung bình các phần tử cực đại
+Ví dụ:
+Input: 96
+1 5 2 6 3 5 1 8 8
+Output: 16
+5.333333
