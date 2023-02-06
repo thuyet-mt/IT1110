7ab262440d99afd9d0c7187b4edfa04292f6ef43
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-02-06 10:23:55 +0700

    Create Bai5.c

---
+++ Midterm & Final Test Code/Bai5.c
@@ -0,0 +1,3 @@
+Hãy nhập vào một xâu không quá 30 ký tự. Hãy cho biết trong xâu đã nhập có bao nhiêu ký tự là chữ cái viết hoa.
+Dữ liệu đầu vào: Một xâu có không quá 30 kí tự
+Dữ liệu đầu ra: Duy nhất một số nguyên, là số ký tự viết hóa trong xâu vừa nhập
