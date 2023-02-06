e97025efb62e4fe91507a9de3bb1756590b35782
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-02-06 10:24:16 +0700

    Create Bai6.c

---
+++ Midterm & Final Test Code/Bai6.c
@@ -0,0 +1,8 @@
+Hãy nhập vào một mảng (không quá 100 phần tử) các số nguyên, hãy tính và đưa ra màn hình số lượng phần tử trong mảng nhỏ hơn, bằng và lớn hơn giá trị của phần tử đầu.
+Dữ liệu đầu vào:
+- Dòng đầu tiên cho biết số phần tử của mảng N (1<=N<=100)
+- Dòng thứ hai là N số nguyên, mỗi số cách nhau một dấu cách
+Kết quả đưa ra: Một dòng gồm 3 số nguyên cách nhau một dấu cách
+- Số thứ nhất: Số phần tử trong mảng có giá trị nhỏ hơn giá trị phần tử đầu
+- Số thứ hai: Số phần tử trong mảng có giá trị bằng hơn giá trị phần tử đầu-
+Số thứ ba: Số phần tử trong mảng có giá trị lớn hơn giá trị phần tử đầu
