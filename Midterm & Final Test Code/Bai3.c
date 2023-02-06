f8696c27046d179d9a12acf1ba8c65eb5a9d8202
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-02-06 10:22:36 +0700

    Create Bai3.c

---
+++ Midterm & Final Test Code/Bai3.c
@@ -0,0 +1,16 @@
+Viết chương trình nhận vào một số tự nhiên n (n nằm trong khoảng biểu diễn của int)
+- In ra số tự nhiên lớn nhất nhỏ hơn hoặc bằng n và là tích của hai số chẵn liên tiếp.
+- In ra số tự nhiên lớn nhất nhỏ hơn hoặc bằng n và là số chính phương.
+- Nếu phần nào không tồn tại số thỏa mãn, in ra ERROR.
+- Nếu 2 phần đều không tồn tại số thỏa mãn hoặc n<0 sẽ in ra
+ERROR
+ERROR
+- Ví dụ
+Input:
+53
+Output:
+48
+49
+Giải thích ví dụ: input: 53
+- Output 1: 48(6*8=48<53, 8*10=80>53)
+- Output 2: 49(7*7=49<53, 8*8=64>53)
