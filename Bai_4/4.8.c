71201ffdcf75fa65be1e2da01a8f35d59058b53a
thuyet-mt14072508 <thuyet.mt1933192@gmail.com>
2023-12-06 16:18:37 +0700

    Update 4.8.c

--- Bai_4/4.8.c
+++ Bai_4/4.8.c
@@ -16,10 +16,48 @@ For example:
             1                   | 2.500000
             1.5                 |
             
-
-
+Cách làm tuần tự không dùng hàm: 
 #include<stdio.h>
 #include<math.h>
+
+int main(){
+    double S = 0, x = 0;
+    int n = 0, i = 0;
+    double factorial = 1;  // Biến lưu giai thừa
+
+    // Nhập giá trị của n và x
+    scanf("%d", &n);
+    scanf("%lf", &x);
+
+    // Kiểm tra nếu n âm, in ra thông báo lỗi và kết thúc chương trình
+    if (n < 0){
+        printf("Error");
+        return 0;
+    }
+
+    for (i = 0; i <= n; i++){
+        if (i > 0) {
+            // Tính giai thừa cho số i trong vòng lặp
+            factorial *= i;
+        }
+        // Tính giá trị của chuỗi số học theo công thức
+        S =S + pow(x, i) / factorial;
+    }
+
+    // In giá trị của S ra màn hình
+    printf("%lf\n", S);
+
+    return 0;
+}
+/*Chúng ta sử dụng biến factorial để tính giai thừa của mỗi số i trong vòng lặp. 
+Mỗi khi i tăng lên, biến factorial sẽ nhân với i, tạo ra giai thừa mới cho số i.*/
+
+
+Cách sử dụng hàm:
+#include<stdio.h>
+#include<math.h>
+
+// Hàm này nhận vào một số nguyên aa và trả về giai thừa của aa. Nếu aa bằng 0, nó trả về 1.
 int factorial(int a){
     int factorial = 1;
     if (a == 0){
@@ -48,3 +86,5 @@ int main(){
     }
     printf("%lf\n", S);
 }
+
+
